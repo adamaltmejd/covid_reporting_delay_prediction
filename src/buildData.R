f507712b5b636ae318b6f524d99699cf59af34cd
Adam Altmejd <adam@altmejd.se>
2020-05-18 11:52:31 +0200

    adapted code to work also on my computer

--- src/buildData.R
+++ src/buildData.R
@@ -4,6 +4,7 @@
 library(Matrix)
 library(stringr)
 library(dplyr)
+library(fst)
 
 # source("src/stolen_function.R")
 
@@ -25,11 +26,16 @@ library(dplyr)
 # death_dt <-death_dt[!is.na(date) & publication_date > "2020-04-02" & date > "2020-04-02"]
 
 deaths_dt <- read_fst(file.path("data", "processed", "deaths_dt.fst"), as.data.table = TRUE)
-
+deaths_dt <- deaths_dt[date > "2020-04-01", .(date, publication_date, N)]
 ##
 #only relevant stuff below
 ##
-res2 <- death_dt %>% tidyr::spread(publication_date, N)
+res2 <- deaths_dt %>% tidyr::spread(publication_date, N)
+
+deaths_dt[, .N, date]
+deaths_dt[is.na(date), .N]
+deaths_dt[date < "2020-03-11"]
+
 detected <- as.matrix(res2[,2:dim(res2)[2]])
 detected <- cbind(matrix(NA,dim(detected)[1],dim(detected)[1]-dim(detected)[2] ) , detected)
 detected[lower.tri(detected)]=NA
@@ -37,6 +43,7 @@ colnames(detected) <- c(res2[,1]$date)
 d_ <- diag(detected)
 d_[is.na(d_)] <- 0
 diag(detected) <- d_
-result = list(detected = detected, dates = res2[,1]$date, dates_report=unique(death_dt$publication_date))
-save(result, file = paste(path.to.files,"/result.RData",sep=""))
+
+result <- list(detected = detected, dates = res2[,1]$date, dates_report=unique(deaths_dt$publication_date))
+saveRDS(result, file.path("data", "processed", "processed_data.rds"))
 
