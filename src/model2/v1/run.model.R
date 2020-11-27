a73417209f03e96fac0c649efd32999caee2ebc4
Adam Altmejd <adam@altmejd.se>
2020-11-27 16:37:22 +0100

    started adding code to functions

--- src/model2/v1/run.model.R
+++ src/model2/v1/run.model.R
@@ -1,28 +1,69 @@
 graphics.off()
-source(file.path("src","model2","v1","model.R"))
-library(numDeriv)
-library(invgamma)
+
 ##
 # data input
-## 
-sim <- 400
-data <- readRDS(file.path("data", "processed", "processed_data.rds"))
-j <- dim(data$detected)[1]
-start_ = j - 31 # run the last 31 days
+##
 
-report_cleaned <- report_clean(data$detected[start_:j,start_:j],data$dates[start_:j])
-new_cases <- newCases(report_cleaned)
-rownames(new_cases) <- as.character(data$dates[start_:j])
-colnames(new_cases) <- as.character(data$dates[start_:j])
+run_model_default <- function(state) {
 
-model_parameters <- list(sim           = sim,
-                         burnin        = ceiling(0.5*sim),
-                         N.days.fixed  =  3,
-                         quantile      = c(0.1,0.9))
+    data <- readRDS(file.path("data", "processed", "processed_data.rds"))
 
-prior_list <- list(mu_beta    = c(0,0,0),
-                   Sigma_beta = 1/2*diag(3),
-                   a_sigma    = c(3,3),
-                   b_sigma    = c(5/2,5/2))
+    model_parameters <- list(sim           = 400,
+                             burnin        = ceiling(0.5*sim),
+                             N.days.fixed  =  3,
+                             quantile      = c(0.1,0.9))
 
-result <- model(new_cases, model_parameters, prior_list)
+    prior_list <- list(mu_beta    = c(0,0,0),
+                       Sigma_beta = 1/2*diag(3),
+                       a_sigma    = c(3,3),
+                       b_sigma    = c(5/2,5/2))
+
+    return(run_model(data, state, model_parameters, prior_list))
+}
+
+run_model <- function(data, state, model_parameters, prior_list) {
+    source(file.path("src", "model2", "v1", "model.R"))
+    require(numDeriv)
+    require(invgamma)
+
+    j <- grep(state, data$dates) # ! Is this correct?
+
+    start_ = j - 31 # run the last 31 days
+
+    report_cleaned <- report_clean(data$detected[start_:j,start_:j],data$dates[start_:j])
+    new_cases <- newCases(report_cleaned)
+
+    rownames(new_cases) <- as.character(data$dates[start_:j])
+    colnames(new_cases) <- as.character(data$dates[start_:j])
+
+    model(new_cases, model_parameters, prior_list)
+}
+
+fetch_plot_data <- function(model_results, deaths_dt, target_lag = 14) {
+    source("src/util/util.scores.R")
+    out <- as.data.table(model_results$Npost)
+
+    out[, date := as.Date(dates)]
+    out[, dates := NULL]
+
+    target <- deaths_dt[days_lag == target_lag, .(date, target = N)]
+
+    # TODO: need to fix deaths_dt so that it contains all days lag, before it was collapsed by work day
+
+    # merge(out, target, by = "date", all.x = TRUE)
+
+    # deaths_dt[date == "2020-10-28"]
+    # deaths_dt[date == "2020-11-12"]
+
+    # out[, SCRPS := SCRPS()]
+
+    # SCRPS <- function(y, mu, sigma)
+
+}
+
+
+a <- run_model_default("2020-11-26")
+b <- run_model_default("2020-06-20")
+deaths_dt <- read_fst("data/processed/deaths_dt.fst", as.data.table = TRUE)
+
+
